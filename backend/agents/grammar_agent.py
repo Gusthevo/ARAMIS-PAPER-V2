e5e8ef5b6a1808c2a9176be1f5d139f0dd888445
Gustavo Campelo <gustavocraft321@gmail.com>
2025-12-18 13:34:55 -0300

    feat: :sparkles: Agents MVP update
    Atualização dos agentes conectados com a api do hugging face, para rodar nosso modelo open-source

--- backend/agents/grammar_agent.py
+++ backend/agents/grammar_agent.py
@@ -1,12 +1,12 @@
 import os
 import json
 import re
-from datetime import datetime
 import time
 from scripts.prompt_manager import render_prompt
 from agno.agent import Agent
-from agno.models.openai import OpenAIChat
-from agno.models.google import Gemini
+#from agno.models.openai import OpenAIChat
+from agno.models.huggingface import HuggingFace
+#from agno.models.google import Gemini
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,10 +26,13 @@ class GrammarCorrectionAgent:
             "../prompts/raw_prompts/v3/grammatical_correction_v3.txt"
         )
 
-        self.model = OpenAIChat(
-            id="gpt-4o",
-            #base_url=os.getenv("LMSTUDIO_BASE_URL"),
-            api_key=os.getenv("OPENAI_API_KEY"),
+        self.model = HuggingFace(
+            id="openai/gpt-oss-20b",
+            #max_tokens = 65536,
+            #base_url=os.getenv("GROQ_BASE_URL"),
+            api_key=os.getenv("HUGGINGFACE_API_KEY"),
+
+            #api_key= os.getenv("OPENAI_API_KEY")
         )
 
     async def analyze(self, text: str, context: dict) -> dict:
@@ -37,7 +40,7 @@ class GrammarCorrectionAgent:
         Método chamado pela orquestração.
         Gera o prompt final, executa o agente e retorna JSON limpinho.
         """
-        start_time = time.monotonic()
+        #start_time = time.monotonic()
 
         # 1. Renderiza o prompt baseado nos dados do frontend
         instructions = render_prompt(self.instructions_file, {
@@ -64,9 +67,9 @@ class GrammarCorrectionAgent:
 
         result = json.loads(json_cleaned)
 
-        end_time = time.monotonic()
-        duration = end_time - start_time
-
+        #end_time = time.monotonic()
+        #duration = end_time - start_time
+        
         # 3. Normaliza a resposta para o formato esperado pelo orquestrador
         return {
             "corrections": result,
--- backend/agents/logical_agent.py
+++ backend/agents/logical_agent.py
@@ -1,12 +1,11 @@
 import os
 import json
 import re
-from datetime import datetime
-import time
 from scripts.prompt_manager import render_prompt
 from agno.agent import Agent
-from agno.models.openai import OpenAIChat
-from agno.models.google import Gemini
+#from agno.models.openai import OpenAIChat
+#from agno.models.google import Gemini
+from agno.models.huggingface import HuggingFace
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,10 +25,13 @@ class LogicalAgent:
             "../prompts/raw_prompts/v3/logical_flow_v3.txt"
         )
 
-        self.model = OpenAIChat(
-            id="gpt-4o",
-            #base_url=os.getenv("LMSTUDIO_BASE_URL"),
-            api_key=os.getenv("OPENAI_API_KEY"),
+        self.model = HuggingFace(
+            id="openai/gpt-oss-20b",
+            #max_tokens = 65536,
+            #base_url=os.getenv("GROQ_BASE_URL"),
+            api_key=os.getenv("HUGGINGFACE_API_KEY"),
+
+            #api_key= os.getenv("OPENAI_API_KEY")
         )
 
     async def analyze(self, text: str, context: dict) -> dict:
@@ -37,7 +39,7 @@ class LogicalAgent:
         Método chamado pela orquestração.
         Gera o prompt final, executa o agente e retorna JSON limpinho.
         """
-        start_time = time.monotonic()
+        #start_time = time.monotonic()
 
         # 1. Renderiza o prompt baseado nos dados do frontend
         instructions = render_prompt(self.instructions_file, {
@@ -64,8 +66,8 @@ class LogicalAgent:
 
         result = json.loads(json_cleaned)
 
-        end_time = time.monotonic()
-        duration = end_time - start_time
+        #end_time = time.monotonic()
+        #duration = end_time - start_time
 
         # 3. Normaliza a resposta para o formato esperado pelo orquestrador
         return {
